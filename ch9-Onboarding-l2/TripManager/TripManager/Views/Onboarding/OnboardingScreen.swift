5730ac58da95da78c5f8e8f873d28a8eb0dbc725
thomas <lacan.thomas@gmail.com>
2024-05-19 16:48:43 -0500

    ch9-l2

--- ch9-Onboarding-l2/TripManager/TripManager/Views/Onboarding/OnboardingScreen.swift
+++ ch9-Onboarding-l2/TripManager/TripManager/Views/Onboarding/OnboardingScreen.swift
@@ -10,14 +10,55 @@ import SwiftUI
 struct OnboardingScreen: View {
   struct ViewStyles {
     static let stepImageBottomSpacing: CGFloat = 80
+    static let tabViewBottomSpacing: CGFloat = 100
+    static let stepBottomSpacing: CGFloat = 60
   }
 
   @State var index: Int = 0
   let stepsData = OnboardingStepData.allValues()
 
   var body: some View {
+    ZStack {
+      Color(R.color.mainBackground.name).ignoresSafeArea()
+      ZStack(alignment: .bottom) {
+        TabView(selection: $index) {
+          ForEach(stepsData) { stepData in
+            stepView(data: stepData)
+              .ignoresSafeArea(edges: .top)
+              .tag(stepData.id)
+              .padding(.bottom, ViewStyles.stepBottomSpacing)
+          }
+        }
+        .padding(.bottom, ViewStyles.tabViewBottomSpacing)
+        .ignoresSafeArea(edges: .top)
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+        VStack {
+          buttonsView()
+            .padding(.top, AppStyles.Padding.small16.rawValue)
+            .padding(.bottom, AppStyles.Padding.big32.rawValue)
+        }
+      }
+    }
+  }
 
-    stepView(data: OnboardingStepData.allValues()[0])
+  @ViewBuilder
+  func buttonsView() -> some View {
+    HStack {
+      Button(action: {
+        print("skip")
+      }, label: {
+        Text(R.string.localizable.onboardingButtonSkip)
+      }).buttonStyle(SecondaryButton())
+
+      Button(action: {
+        withAnimation(.default) {
+          index += 1
+        }
+      }, label: {
+        Text(R.string.localizable.onboardingButtonNext)
+      }).buttonStyle(PrimaryButton())
+    }
   }
 
   @ViewBuilder
