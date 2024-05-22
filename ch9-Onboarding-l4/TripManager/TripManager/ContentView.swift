03093fe4c346bfab86808924161bded298cd3455
thomas <lacan.thomas@gmail.com>
2024-05-22 18:38:28 -0500

    ch9-Onboarding-l4

---
+++ ch9-Onboarding-l4/TripManager/TripManager/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  TripManager
+//
+//  Created by thomas lacan on 19/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+  var body: some View {
+    VStack {
+    }
+  }
+}
+
+#Preview {
+  ContentView()
+}
