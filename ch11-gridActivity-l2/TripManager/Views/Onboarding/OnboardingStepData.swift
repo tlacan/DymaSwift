44e913a132e29e8b4bc0f2969fdf689065d983aa
thomas <lacan.thomas@gmail.com>
2024-06-18 17:21:30 +0200

    ch11-gridActivity-l2

---
+++ ch11-gridActivity-l2/TripManager/Views/Onboarding/OnboardingStepData.swift
@@ -0,0 +1,23 @@
+//
+//  OnboardingStepData.swift
+//  TripManager
+//
+//  Created by thomas lacan on 17/05/2024.
+//
+
+import Foundation
+
+struct OnboardingStepData: Identifiable {
+  let id: Int
+  let image: String
+  let title: String
+  let description: String
+
+  static func allValues() -> [OnboardingStepData] {
+    [
+      OnboardingStepData(id: 0, image: R.image.onboarding1.name, title: R.string.localizable.onboardingStep1Title(), description: R.string.localizable.onboardingStep1Description()),
+      OnboardingStepData(id: 1, image: R.image.onboarding2.name, title: R.string.localizable.onboardingStep2Title(), description: R.string.localizable.onboardingStep2Description()),
+      OnboardingStepData(id: 2, image: R.image.onboarding3.name, title: R.string.localizable.onboardingStep3Title(), description: R.string.localizable.onboardingStep3Description())
+    ]
+  }
+}
